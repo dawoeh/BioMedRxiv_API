d96170638261bc201cd66aed50f397c7481abe3e
David Wöhlert <david.woehlert@gmx.de>
2021-07-21 23:24:15 +0200

    finished api calls; start data exploration/cleaning

--- retrieval.py
+++ retrieval.py
@@ -1,13 +1,10 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[23]:
 
 
-import requests
-import json
-from time import sleep
-
+import re
 from utils.utils import *
 
 import pandas as pd
@@ -18,39 +15,108 @@ import pandas as pd
 
 server = 'biorxiv'
 date_start = '2021-06-01'
-date_end = '2021-06-30'
+date_end = '2021-06-01'
 data_biorxiv = date_rxiv(server, date_start, date_end)
+data_biorxiv.drop_duplicates(subset = ['doi'], keep = 'last', inplace = True)
+data_biorxiv
 
 
 # In[3]:
 
 
-data_biorxiv
+doi_article = data_biorxiv.at[0, 'doi']
+server = 'biorxiv'
+data_doi = article_detail(server, doi_article)
+data_doi
+
+
+# In[4]:
+
+
+server = 'medrxiv'
+data_medrxiv = date_rxiv(server, date_start, date_end)
+data_medrxiv.drop_duplicates(subset = ['doi'], keep = 'last', inplace = True)
+data_medrxiv
+
+
+# In[5]:
+
+
+date_start = '2021-06-01'
+date_end = '2021-06-01'
+data_pub_biorxiv = date_published_article(date_start, date_end)
+data_pub_biorxiv
 
 
 # In[6]:
 
 
-data_biorxiv.loc[data_biorxiv['version'] != '1', 'version']
+publisher_id = '10.15252'
+date_start = '2020-06-01'
+date_end = '2021-06-01'
+data_pub_biorxiv = date_publisher_detail(publisher_id, date_start, date_end)
+data_pub_biorxiv
 
 
 # In[7]:
 
 
-server = 'medrxiv'
-data_medrxiv = date_rxiv(server, date_start, date_end)
-data_medrxiv
+data_stats = biorxiv_stats()
+data_stats
 
 
-# In[9]:
-
-
-data_medrxiv.loc[data_medrxiv['version'] != '1', 'version']
-
-
-# In[10]:
+# In[8]:
 
 
 data_biorxiv.to_csv('processed/biorxiv_june.csv', index=False)
 data_medrxiv.to_csv('processed/medrxiv_june.csv', index=False)
+
+
+# In[22]:
+
+
+print(data_biorxiv.at[5, 'abstract'])
+
+
+# In[40]:
+
+
+first_words = set()
+for i, abstract in enumerate(data_biorxiv['abstract'].tolist()):
+    hits = []
+    sentences = re.split('\n', abstract)
+    for sent in sentences:
+        try:
+            first = sent.split()[0]
+        except:
+            first = ''
+        hits.append(re.findall(r'\w*[A-Z]\w*[A-Z]\w*', first))
+    #if hit:
+    if sum([len(elem) for elem in hits]) > 0:
+        for elem in hits:
+            if len(elem) > 0:
+                first_words.add(elem[0])
+print(first_words)
+
+
+# In[51]:
+
+
+term_list = ['Results', 'Contribution', 'Highlights', 'Background', 'Conclusions', 'Introduction', 'Motivation', 'Methods', 'Teaser',              'Abstracts', 'Conclusion', 'IMPORTANCE', 'SUMMARY', 'Finding', 'Availability']
+for word in first_words:
+    present = 0
+    for term in term_list:
+        if term in word:
+            present = 1
+            break
+    if present == 0:
+        print(word)
+###FIND GRAPHICAL ABSTRACT AND REMOVE!
+###AUTHOR STATEMENT, IMPACT STATEMENT, SIGNIFICANCE STATEMENT, DATA STATEMENT
+
+
+# In[ ]:
+
+
+
 
--- utils/utils.py
+++ utils/utils.py
@@ -5,7 +5,7 @@ from time import sleep
 
 import pandas as pd
 
-def date_rxiv(server, from_date, to_date, result_start=1):
+def date_rxiv(server, from_date, to_date, result_start=0):
     data = pd.DataFrame()
     while True:
         response = requests.get(f'https://api.biorxiv.org/details/{server}/{from_date}/{to_date}/{result_start}')
@@ -21,4 +21,64 @@ def date_rxiv(server, from_date, to_date, result_start=1):
             data = data.append(data_temp).reset_index(drop=True)
         result_start += 100
         sleep(1)
+    return data
+
+def article_detail(server, doi):
+    data = pd.DataFrame()
+    response = requests.get(f'https://api.biorxiv.org/details/{server}/{doi}')
+    doc = response.json()   
+    for article in doc['collection']:
+        data_temp = pd.DataFrame(list(article.items()))
+        data_temp = data_temp.transpose()
+        data_temp.columns = article.keys()
+        data_temp.drop(0, inplace=True)
+        data = data.append(data_temp).reset_index(drop=True)
+    return data
+
+def date_published_article(from_date, to_date, result_start=0):
+    data = pd.DataFrame()
+    while True:
+        response = requests.get(f'https://api.biorxiv.org/pub/{from_date}/{to_date}/{result_start}')
+        doc = response.json()   
+        result_count = len(doc['collection'])
+        if result_count == 0:
+                break
+        for article in doc['collection']:
+            data_temp = pd.DataFrame(list(article.items()))
+            data_temp = data_temp.transpose()
+            data_temp.columns = article.keys()
+            data_temp.drop(0, inplace=True)
+            data = data.append(data_temp).reset_index(drop=True)
+        result_start += 100
+        sleep(1)
+    return data
+
+def date_publisher_detail(publisher_id, from_date, to_date, result_start=0):
+    data = pd.DataFrame()
+    while True:
+        response = requests.get(f'https://api.biorxiv.org/publisher/{publisher_id}/{from_date}/{to_date}/{result_start}')
+        doc = response.json()   
+        result_count = len(doc['collection'])
+        if result_count == 0:
+                break
+        for article in doc['collection']:
+            data_temp = pd.DataFrame(list(article.items()))
+            data_temp = data_temp.transpose()
+            data_temp.columns = article.keys()
+            data_temp.drop(0, inplace=True)
+            data = data.append(data_temp).reset_index(drop=True)
+        result_start += 100
+        sleep(1)
+    return data
+
+def biorxiv_stats(interval = 'm'):
+    data = pd.DataFrame()
+    response = requests.get(f'https://api.biorxiv.org/sum/{interval}')
+    doc = response.json()   
+    for element in doc['bioRxiv content statistics']:
+        data_temp = pd.DataFrame(list(element.items()))
+        data_temp = data_temp.transpose()
+        data_temp.columns = element.keys()
+        data_temp.drop(0, inplace=True)
+        data = data.append(data_temp).reset_index(drop=True)
     return data
