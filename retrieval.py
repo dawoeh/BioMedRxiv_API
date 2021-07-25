167cbb6b213705ebbc7c2c0a40e872658fa4c875
David Wöhlert <david.woehlert@gmx.de>
2021-07-25 14:25:53 +0200

    update formatting removal

--- retrieval.py
+++ retrieval.py
@@ -18,6 +18,7 @@ date_start = '2021-06-01'
 date_end = '2021-06-30'
 data_biorxiv = date_rxiv(server, date_start, date_end)
 data_biorxiv.drop_duplicates(subset = ['doi'], keep = 'last', inplace = True)
+data_biorxiv.reset_index(drop=True, inplace=True)
 data_biorxiv
 
 
@@ -33,9 +34,12 @@ data_doi
 # In[4]:
 
 
+date_start = '2021-06-01'
+date_end = '2021-06-30'
 server = 'medrxiv'
 data_medrxiv = date_rxiv(server, date_start, date_end)
 data_medrxiv.drop_duplicates(subset = ['doi'], keep = 'last', inplace = True)
+data_medrxiv.reset_index(drop=True, inplace=True)
 data_medrxiv
 
 
@@ -68,7 +72,7 @@ data_stats
 # In[8]:
 
 
-print(data_biorxiv.at[5, 'abstract'])
+print(data_biorxiv.at[10, 'abstract'])
 
 
 # In[9]:
@@ -103,17 +107,24 @@ print(abstract_list[195])
 
 abstract_list = data_biorxiv['abstract'].tolist()
 
-header_list = ['Results', 'Contribution', 'Highlights', 'Background', 'Conclusions', 'Introduction', 'Motivation', 'Methods', 'Teaser',             'Abstracts', 'Conclusion', 'IMPORTANCE', 'SUMMARY', 'Finding', 'Availability', 'Highlight', 'Importance', 'Summary', 'RESULTS',             'Rationale', 'Aim', 'Significance', 'Objectives', 'Purpose', 'Synopsis', 'METHODS', 'SIGNIFICANCE', 'Objectives', 'Design',             'Objective', 'Approach', 'Methodology', 'HIGHLIGHTS', 'INTRODUCTION', 'Abstract', 'CONCLUSIONS', 'Software', 'Interpretations',             'FUNDING', 'Summery', 'Perspective', 'Discussion', 'CONCLUSION', 'DISCUSSION', 'Funding', 'Advances', 'Classification', 'SYNOPSIS',             'BACKGROUND', 'Method', 'Innovation', 'PREMISE', 'Implications', 'STATEMENT', 'Contact', 'Location', 'Outlook', 'TEASER',             'Interpretation', 'eTOC', 'Result', 'TABLE', 'Taxon', 'Measurements', 'Participants', 'Sentence', 'Materials',             'PURPOSE', 'Context', 'SUMMMARY', 'Hypothesis', 'Keypoitns', 'Implementation', 'Limitations', 'OBJECTIVE',             'Precis', 'Questions', 'Database', 'Paragraph', 'PARAGRAPH', 'DECLARATIONS', 'ABSTRACT', 'HIGHLIGHT']
+header_list = ['Results', 'Contribution', 'Highlights', 'Background', 'Conclusions', 'Introduction', 'Motivation', 'Methods', 'Teaser',             'Abstracts', 'Conclusion', 'IMPORTANCE', 'SUMMARY', 'Finding', 'Availability', 'Highlight', 'Importance', 'Summary', 'RESULTS',             'Rationale', 'Aim', 'Significance', 'Objectives', 'Purpose', 'Synopsis', 'METHODS', 'SIGNIFICANCE', 'Objectives', 'Design',             'Objective', 'Approach', 'Methodology', 'HIGHLIGHTS', 'INTRODUCTION', 'Abstract', 'CONCLUSIONS', 'Software', 'Interpretations',             'FUNDING', 'Summery', 'Perspective', 'Discussion', 'CONCLUSION', 'DISCUSSION', 'Funding', 'Advances', 'Classification', 'SYNOPSIS',             'BACKGROUND', 'Method', 'Innovation', 'PREMISE', 'Implications', 'STATEMENT', 'Contact', 'Location', 'Outlook', 'TEASER',             'Interpretation', 'eTOC', 'Result', 'TABLE', 'Taxon', 'Measurements', 'Participants', 'Sentence', 'Materials',             'PURPOSE', 'Context', 'SUMMMARY', 'Hypothesis', 'Keypoitns', 'Implementation', 'Limitations', 'OBJECTIVE', 'Statement',             'Precis', 'Questions', 'Database', 'Paragraph', 'PARAGRAPH', 'DECLARATIONS', 'ABSTRACT', 'HIGHLIGHT', 'Application',              'OUTCOME', 'Paragraph', 'Premise', 'Cover', 'Rational', 'ARTICLE', 'SETTING', 'RESUMEN', 'Repositories', 'SAMENVATTING',              'Animals', 'Disclaimer', 'Impact', 'Procedures', 'Foreword', 'EXPOSURES', 'FINDINGS', 'OBJECTIVES', 'Backgroud', 'Assessment',               'DESIGN', 'ABSRTRACT', 'License', 'SUMARY', 'Conclusions', 'ABSRACT', 'Originality/Value', 'Conclusions/interpretation', 'Problem',               'PURPOSE/HYPOTHESIS', 'RATIONALE', 'PARTICIPANTS', 'ASSESSMENT', 'Recommendations', 'Meaning', 'METHOD', 'LIMITATIONS', 'Category',               'Abbreviations', 'IMPRTANCE', 'Methos', 'Details', 'Reproducibility', 'Conclussion', 'Disclosures', 'Recommendation', 'Outcomes',               'REPOSITORIES', 'RESUME', 'IMPACT', 'Availablity', 'Intervention', 'Notice', 'Resources', 'Setting', 'Synthesis', 'Digest',               'SUPPORT', 'Abtsract', 'MANUSCRIPT', 'Blurp', 'ABSTACT', 'KEYPOINTS', 'ABSTARCT', 'Authorship', 'ABTRAST', 'Scope']
 
-markup_list =  ['C_LIO_LI', 'O_LI', 'O_SCPLOWBSTRACTC_SCPLOW', 'AO_SCPLOWUTHORC_SCPLOW', 'SO_SCPLOWUMMARYC_SCPLOW', 'SO_SCPLOWIGNIFICANCEC_SCPLOWO_SCPCAP',                 'C_TABLECAPTION', 'IO_SCPLOWMPORTANCEC_SCPLOW', 'C_LI', 'C_TEXTBOX', 'O_SCPLOWLC_SCPLOW', 'C_SCPCAPO_SCPLOWPOINTSC_SCPLOW',                 'KO_SCPLOWEYC_SCPLOWO_SCPCAP', 'C_QD', 'C_ST_ABSO_LI', 'O_ST_ABS', 'C_ST_ABS', 'HO_SCPLOWIGHLIGHTSC_SCPLOW',                 'C_SCPCAPO_SCPLOWC_SCPLOW', 'O_TEXTBOX', 'O_SCPCAP', 'HIGHLIGHTSO_LI', 'HIGHLIGHTO_LI', 'C_SCPCAPO_SCPLOWSTATEMENTC_SCPLOW']
+markup_list =  ['C_LIO_LI', 'O_LI', 'C_TABLECAPTION', 'C_LI', 'C_TEXTBOX', 'O_QD', 'C_QD', 'C_ST_ABSO_LI', 'O_TEXTBOX', 'O_SCPCAP', 'C_SCPCAP', 'HIGHLIGHTSO_LI', 'HIGHLIGHTO_LI', 'O_LST', 'C_LST'] 
 
-statement_list = ['AUTHOR STATEMENT', 'IMPACT STATEMENT', 'SIGNIFICANCE STATEMENT', 'DATA STATEMENT', 'ARTICLE SUMMARY', 'AUTHOR SUMMARY',                   'CONFLICT OF INTEREST', 'IN BRIEF', 'METHODOLOGY/PRINCIPAL FINDINGS', 'ABSTRACT AND KEYWORDS', 'PROGRAM SUMMARY',                   'ARTICLE SUMMARY', 'NEW & NOTEWORTHY', 'Graphical abstract', 'GRAPHICAL ABSTRACT', 'HIGHLIGHT STATEMENT', 'Funding Information',                   'SUMMARY STATEMENT', 'DATA SUMMARY', 'SUMMARY PARAGRAPH']
-
+statement_list = ['AUTHOR STATEMENT', 'IMPACT STATEMENT', 'SIGNIFICANCE STATEMENT', 'DATA STATEMENT', 'ARTICLE SUMMARY', 'AUTHOR SUMMARY',                   'CONFLICT OF INTEREST', 'IN BRIEF', 'METHODOLOGY/PRINCIPAL FINDINGS', 'ABSTRACT AND KEYWORDS', 'PROGRAM SUMMARY',                   'ARTICLE SUMMARY', 'NEW & NOTEWORTHY', 'Graphical abstract', 'GRAPHICAL ABSTRACT', 'HIGHLIGHT STATEMENT', 'Funding Information',                   'SUMMARY STATEMENT', 'DATA SUMMARY', 'SUMMARY PARAGRAPH', 'One Sentence Summary', 'CONDENSED ABSTRACT', 'Sentence Summary', 'Software Availability',                  'Statement of Significance', 'Summary statement', 'Summary Paragraph', 'SUBMISSION CATEGORY', 'eTOC Summary', 'Impact Summary', 'Summary sentence',                  'Impact statement', 'Method of Study', 'STATISTICAL TESTS', 'Statistical Tests', 'STUDY TYPE', 'Summary Sentence', 'Significance statement',                  'LIMITATIONS, REASONS FOR CAUTION', 'WIDER IMPLICATIONS OF THE FINDINGS', 'STUDY FUNDINGS/COMPETING INTEREST(S)', 'EXPERIMENTAL APPROACH',                  'KEY RESULTS', 'CONCLUSIONS and IMPLICATIONS', 'BACKGROUND AND PURPOSE', 'AUTHORS SUMMARY', 'DESIGN SETTINGS AND PARTICIPANTS',                  'MAIN OUTCOME AND MEASURES', 'CONCLUSION AND RELEVANCE', 'STRUCTURED ABSTRACT INTRODUCTION', 'SIGNIFICANCE STATMENT', 'Summary Statement',                  'Abstract Importance', 'Conclusions /Significance', 'Methodology / principal finding', 'Back ground', 'Highlight Summary',                  'AVAILABILITY AND IMPLEMENTATION', 'Highlights statement', 'MPACT SUMMARY', 'BACKGROUND & OBJECTIVES', 'BACKGROUND AND HYPOTHESIS',                  'BACKGROUND & AIMS', 'ABSTRACT BACKGROUND & AIMS', 'APPROACH AND RESULTS', 'ABSTRACT  OBJECTIVES', 'eTOC BLURB', 'Author Summary',                  'STATEMENT OF SIGNIFICANCE', 'Software availability', 'eTOC Blurb', 'GRAPHICAL SUMMARY', 'RESEARCH IN CONTEXT', 'CONTRIBUTION TO THE FIELD',                  'TRANSLATIONAL PERSPECTIVE', 'COMPARISON WITH EXISTING METHODS', 'MATERIALS & METHODS', 'EXPERIMENTAL RESULTS', 'SCIENCE FOR SOCIETY',                  'Method Summary', 'RESOURCE SHARING', 'TOC Graphic', 'CLINICAL SIGNIFICANCE', 'ETHICAL STATEMENT', 'Significance Statement',                  'Impact Statement', 'Data Summary']
 
 markup_list.sort(key = len, reverse=True)
 header_list.sort(key = len, reverse=True)
+statement_list.sort(key = len, reverse=True)
+
+markup_short_list = ['O_ST_ABS', 'C_ST_ABS', 'O_SCPLOW', 'C_SCPLOW']
 
 for i, abstract in enumerate(abstract_list):
+    for markup in markup_short_list:
+        if markup == 'O_SCPLOW':
+            abstract = abstract.replace(markup, '')
+        else:
+            abstract = abstract.replace(markup, ' ')
     for markup in markup_list:
         abstract = abstract.replace(markup, ' ')
     for statement in statement_list:
@@ -140,7 +151,7 @@ for i, abstract in enumerate(abstract_list):
             stop = 0
         if 'C_TBL' in word:
             stop = 0        
-        if '\n' in word:
+        if '\n' in word or 'C_FIG' in word or 'C_TBL' in word:
             pointer = 1
         else:
             pointer = 0
@@ -167,7 +178,7 @@ for i, abstract in enumerate(abstract_list):
             if len(elem) > 0:
                 first_words.add(elem[0])
 
-term_list = ['Statement',]
+term_list = []
 for word in first_words:
     present = 0
     for term in term_list:
@@ -183,9 +194,18 @@ for word in first_words:
 
 data_biorxiv_curate = data_biorxiv.copy()
 data_biorxiv_curate['abstract'] = abstract_list
-data_biorxiv.to_csv('processed/biorxiv_june.csv', index=False)
-data_medrxiv.to_csv('processed/medrxiv_june.csv', index=False)
+data_biorxiv.to_csv('raw/biorxiv_june.csv', index=False)
+data_medrxiv.to_csv('raw/medrxiv_june.csv', index=False)
 data_biorxiv_curate.to_csv('processed/biorxiv_june_curate.csv', index=False)
+
+
+# In[14]:
+
+
+response = requests.get('https://api.biorxiv.org/details/biorxiv/2021-06-01/2021-06-30/0')
+doc = response.json()
+total_results = int(doc['messages'][0]['total'])
+total_results
 
 
 # In[ ]:
